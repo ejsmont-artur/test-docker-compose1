c265ef5f34b31c9e341556c9c647687e30b51c24
Art <ejsmont.artur@gmail.com>
2016-05-06 16:20:46 +1000

    redis

--- php-image/src/index.php
+++ php-image/src/index.php
@@ -1,6 +1,23 @@
 <?php
 
-system('hostname');
+$host = gethostname();
+
+$redisClient = new Redis();
+$conn = $redisClient->connect('redis', 6379);
+$cachedData = $redisClient->get('cached');
+if (empty($cachedData)) {
+    $data = array($host => microtime(true));
+} else {
+    $data = json_decode($cachedData, true);
+    $data[$host] = microtime(true);
+}
+$redisClient->set('cached', json_encode($data));
+
+echo "<pre>
+Host: $host\n
+Cached data: " . print_r($cachedData, true) . "
+Object data: " . print_r($data, true) . "
+</pre>";
 
 phpinfo();
 
